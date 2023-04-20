580d91c2b05212f961c2b77e78a45fd631664ff0
Singh <suraj.singh4@dxc.com>
2023-04-20 12:30:43 +0530

    updated

---
+++ Largest of three/Largest of three/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+int num1, num2, num3;
+Console.WriteLine("Enter First Number");
+num1 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter Second Number");
+num2 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter Third Number");
+num3 = Convert.ToInt16(Console.ReadLine());
+if (num1 > num2 && num1 > num3)
+{
+    Console.WriteLine("First Number is Largest");
+}
+else if (num2 > num1 && num2 > num3)
+{
+    Console.WriteLine("Second Number is Largest");
+}
+else if (num3 > num1 && num3 > num2)
+{
+    Console.WriteLine("Third Number is Largest");
+}
+else
+{
+    Console.WriteLine("All are equal");
+}
---
+++ Vowel or Not/Vowel or Not/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+Console.WriteLine("Enter an alphabet");
+String ch = Console.ReadLine();
+if ( ch =="a" || ch == "e" || ch == "i" || ch == "o" || ch == "u")
+{
+    Console.WriteLine("Alphabet is vowel");
+}
+else
+{
+    Console.WriteLine("Alphabet is not a vowel");
+}
