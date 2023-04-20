8caac08c99534f6dac8db990fc303118bbc513cf
Singh <suraj.singh4@dxc.com>
2023-04-20 17:08:14 +0530

    Updated

---
+++ Operator/Operator/Program.cs
@@ -0,0 +1,83 @@
+﻿Console.WriteLine("Enter First Number: ");
+
+int a = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Enter Second Number: ");
+
+int b = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Enter the Operator :");
+
+
+
+
+char ch = Convert.ToChar(Console.ReadLine());
+
+
+
+
+switch (ch)
+
+{
+
+    case '+':
+
+        {
+
+            Console.WriteLine("Result is " + (a + b));
+
+            break;
+
+        }
+
+    case '-':
+
+        {
+
+            Console.WriteLine("Result is " + (a - b));
+
+            break;
+
+        }
+
+    case '*':
+
+        {
+
+            Console.WriteLine("Result is " + (a * b));
+
+            break;
+
+        }
+
+    case '/':
+
+        {
+
+            Console.WriteLine("Result is " + (a / b));
+
+            break;
+
+        }
+
+    case '%':
+
+        {
+
+            Console.WriteLine("Result is " + (a % b));
+
+            break;
+
+        }
+
+    default:
+
+        {
+
+            Console.WriteLine("Wrong Choice");
+
+            break;
+
+        }
+
+}
