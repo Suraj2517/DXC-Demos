312e0b98f4144ff2da1538200a8add5039303fc7
Singh <suraj.singh4@dxc.com>
2023-04-21 11:52:55 +0530

    Updated

---
+++ Continue temp/Continue temp/Program.cs
@@ -0,0 +1,11 @@
+﻿String ch;
+    do
+    {
+        Console.WriteLine("Enter the Temperature in Degree Celsius: ");
+        int C = Convert.ToInt16(Console.ReadLine());
+
+        double F = C * 1.8 + 32;
+        Console.WriteLine("Temperature(Celsius): " + C + "     Temperature(Fahrenheit): " + F);
+        Console.WriteLine("Do You Wish to Continue(y/n)?");
+        ch = Console.ReadLine();
+    } while (ch == "y");
---
+++ Sum 5 digit/Sum 5 digit/Program.cs
@@ -0,0 +1,10 @@
+﻿int n, sum = 0, m;
+Console.Write("Enter a 5 digit number: ");
+n = int.Parse(Console.ReadLine());
+while (n > 0)
+{
+    m = n % 10;
+    sum = sum + m;
+    n = n / 10;
+}
+Console.Write("Sum of all 5 digit is = " + sum);
