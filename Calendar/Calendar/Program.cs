a9d956c6b934ce26d9a8507ecda9d7854c9dcf7e
Singh <suraj.singh4@dxc.com>
2023-04-24 12:24:04 +0530

    Updated

---
+++ Calendar/Calendar/Program.cs
@@ -0,0 +1,7 @@
+﻿int m, y, d; 
+Console.WriteLine("Enter month(Press 1 for Jan, 2 for Feb, 12 for Dec......");
+m = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter Year:");
+y = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter Day of week(Press 1 for Mon, 2 for Tue, 7 for Sun......");
+d = Convert.ToInt16(Console.ReadLine());
