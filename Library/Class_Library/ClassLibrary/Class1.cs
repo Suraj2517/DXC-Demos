48a351f76af08ba7377a2dcecbc7a08518f5e520
Singh <suraj.singh4@dxc.com>
2023-04-26 17:39:06 +0530

    Updated

---
+++ Library/Class_Library/ClassLibrary/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace ClassLibrary
+{
+    public class Calculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide(int a, int b)
+        {
+            return (a / b);
+        }
+    }
+}
---
+++ OOP_Demo/OOP_Demo/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace OOP_Demo
+{
+    class Calculator
+    {
+        public void Show()
+        {
+            Console.WriteLine("This is hello from myside");
+        }
+
+        public void SayHello(string name)
+        {
+            Console.WriteLine($"Hello {name}");
+        }
+
+        public void Sum(int a, int b, int c)
+        {
+            Console.WriteLine(a + b + c);
+        }
+
+        public int Cube(int x)
+        {
+            return x * x * x;
+        }
+
+        public void Add(int a, int b = 0, int c = 0)
+        {
+            Console.WriteLine(a + b + c);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.Show();
+            calc.SayHello("Raja");
+            calc.SayHello("M S");
+            calc.Sum(10, 2, 3);
+            int res = calc.Cube(7);
+            Console.WriteLine(res);
+            calc.Add(10, 20);
+        }
+    }
+}
